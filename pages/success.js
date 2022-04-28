dd6746802f1de1b16ff547bf9058f5155c920650
Sami Warraich <samiwarraich51@gmail.com>
2022-04-29 01:32:14 +0500

    initial commit

---
+++ pages/success.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+import { runFireworks } from "../lib/utils";
+import { useStateContext } from "../context/StateContext";
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    runFireworks();
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email for the receipt.</p>
+        <p className="description">
+          If you have any questions, please contact us at{" "}
+          <a className="email" href="mailto:samiwarraich51@gmail.com">
+            samiwarraich51@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
